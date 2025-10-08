5011f6a721cbc9f78d638aba3b197654c125383a
joaopedrosantanamotalol <jp1119174@gmail.com>
2025-10-08 14:40:40 -0300

    layout mais facil de se achar

--- app/home/home.tsx
+++ app/home/home.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { ScrollView, View, Text, ImageBackground } from "react-native";
 import styles from "../../styles/home";
 
@@ -16,7 +17,22 @@ export default function home(){
         <Text style={styles.titulo}>Entenda Melhor nosso Projeto</Text>
         </View>
 
+        <View style={styles.container}>
+
+        <Text style={styles.card}>o VestMath é um projeto que surgiu com a ideia de ajudar estudantes
+que gostariam de  verificar as chances que possuem 
+para entrar na faculdade ou universidade de seus sonhos 
+através de um calculo matemático</Text>
+
+        
+        </View>
+
+        <View style={styles.body_2}>
+        <Text style={styles.titulo}>Como funciona nosso Calculo?</Text>
+        </View>
+
         </ScrollView>
+
         </>
     )
 }
--- styles/home.js
+++ styles/home.js
@@ -13,8 +13,8 @@ body_2:{
 flex: 1,
 alignItems: "center",
 justifyContent: "flex-start",
-height: "100%",
-backgroundColor: "#FFFFFF"
+height: 60,
+backgroundColor: "yellow"
 },
 image_header:{
 width: "100%",
@@ -25,6 +25,25 @@ fontSize: 20,
 color: "#000000",
 textAlign: "center",
 fontWeight: "bold",
-marginTop:10
-}
+marginTop:10,
+},
+container:{
+  justifyContent: "center",
+  alignItems: "center",  // <- centraliza horizontalmente
+  flex:1,
+  backgroundColor: "red",
+  height:0         
+},
+card:{
+  height:100,
+  width:"80%",
+backgroundColor:"#33313B",
+color: "white",
+textAlign:"center",
+verticalAlign:"middle",
+paddingLeft:20,
+paddingRight:20,
+marginBottom:400,
+borderRadius:30,
+},
 });
