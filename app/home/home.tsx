93e86b45cde95bcbc8b6e8329560cc1b56721248
joaopédrosantanamotalol <jp1119174@gmail.com>
2025-10-08 18:06:58 -0300

    update: homeLAyout

--- app/home/home.tsx
+++ app/home/home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, View, Text, ImageBackground } from "react-native";
+import { ImageBackground, ScrollView, Text, View } from "react-native";
 import styles from "../../styles/home";
 
 export default function home(){
@@ -15,20 +15,24 @@ export default function home(){
 
         <View style={styles.body_2}>
         <Text style={styles.titulo}>Entenda Melhor nosso Projeto</Text>
-        </View>
-
-        <View style={styles.container}>
 
         <Text style={styles.card}>o VestMath é um projeto que surgiu com a ideia de ajudar estudantes
 que gostariam de  verificar as chances que possuem 
 para entrar na faculdade ou universidade de seus sonhos 
 através de um calculo matemático</Text>
-
-        
         </View>
 
-        <View style={styles.body_2}>
+        <View style={styles.container}>
         <Text style={styles.titulo}>Como funciona nosso Calculo?</Text>
+        </View>
+
+
+        <View style={styles.body_2}>
+
+        <Text style={styles.card2}>Suas chances = (R+I = X) | (C+I = Y)</Text>
+
+        <Text style={styles.card_lateral}>Suas chances = (R+I = X) | (C+I = Y)</Text>
+
         </View>
 
         </ScrollView>
--- styles/home.js
+++ styles/home.js
@@ -1,49 +1,79 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-header:{
-alignItems: "center",
-justifyContent: "flex-start",
-height:250
-},
+  header: {
+    alignItems: "center",
+    justifyContent: "flex-start",
+    height: 250,
+  },
   scrollContainer: {
-    flexGrow: 1, // faz o conteúdo ocupar pelo menos toda a tela
-},
-body_2:{
-flex: 1,
-alignItems: "center",
-justifyContent: "flex-start",
-height: 60,
-backgroundColor: "yellow"
-},
-image_header:{
-width: "100%",
-height: "100%"
-},
-titulo:{
-fontSize: 20,
-color: "#000000",
-textAlign: "center",
-fontWeight: "bold",
-marginTop:10,
-},
-container:{
-  justifyContent: "center",
-  alignItems: "center",  // <- centraliza horizontalmente
-  flex:1,
-  backgroundColor: "red",
-  height:0         
-},
-card:{
-  height:100,
-  width:"80%",
-backgroundColor:"#33313B",
-color: "white",
-textAlign:"center",
-verticalAlign:"middle",
-paddingLeft:20,
-paddingRight:20,
-marginBottom:400,
-borderRadius:30,
-},
+    flexGrow: 1, // Faz o conteúdo ocupar pelo menos toda a tela
+  },
+  body_2: {
+    height:"15%",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    backgroundColor: "yellow",
+    padding: 0, // Ajustando o padding para um melhor controle
+  },
+  image_header: {
+    width: "100%",
+    height: "100%",
+  },
+  titulo: {
+    fontSize: 20,
+    color: "#000000",
+    textAlign: "center",
+    fontWeight: "bold",
+    marginTop: 10,
+    marginBottom: "auto",
+  },
+  container: {
+    justifyContent: "center",
+    alignItems: "center", // Centraliza horizontalmente
+    backgroundColor: "red",
+    height: "5%",
+  },
+  card: {
+    height: 100,
+    width: "auto",
+    maxWidth:350,
+    backgroundColor: "#33313B",
+    color: "white",
+    textAlign: "center",
+    verticalAlign: "middle",
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginBottom: 400,
+    borderRadius: 30,
+    marginTop:15,
+  },
+  card2: {
+    height: 50,
+    maxWidth:300,
+    width: "auto",
+    backgroundColor: "#33313B",
+    color: "white",
+    textAlign: "center",
+    verticalAlign: "middle",
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginBottom:15,
+    borderRadius: 30,
+    marginTop:15,
+  },
+  card_lateral: {
+    height: 50,
+    maxWidth:250,
+    width: "auto",
+    backgroundColor: "#33313B",
+    color: "white",
+    textAlign: "center",
+    verticalAlign: "middle",
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginBottom: 400,
+    borderRadius: 30,
+    marginTop:15,
+  },
 });
